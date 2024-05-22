33486f6831a502af048e8917aa6480ed9b316cce
KadirovKmck <kadirovmuhammadaziz79@gmail.com>
2024-05-22 14:49:29 +0600

    hb

--- lib/app/views/settings_view/settings_view.dart
+++ lib/app/views/settings_view/settings_view.dart
@@ -1,7 +1,7 @@
-// ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class SettingsView extends StatefulWidget {
   const SettingsView({super.key});
@@ -12,6 +12,90 @@ class SettingsView extends StatefulWidget {
 
 class _SettingsViewState extends State<SettingsView> {
   bool _notificationsEnabled = true;
+  double _rating = 0.0;
+
+  void _showRatingDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Rate Us'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(height: 20),
+              RatingBar.builder(
+                minRating: 1,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemCount: 5,
+                itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                itemBuilder: (context, _) => const Icon(
+                  Icons.star,
+                  color: Colors.amber,
+                ),
+                onRatingUpdate: (rating) {
+                  setState(() {
+                    _rating = rating;
+                  });
+                },
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Submit'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showDeleteAccountDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Delete Account'),
+          content: const Text(
+              'Are you sure you want to delete your account? This action cannot be undone.'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Delete'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _deleteAccount();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deleteAccount() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Account deleted successfully'),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,8 +144,10 @@ class _SettingsViewState extends State<SettingsView> {
             const Divider(color: Colors.grey),
             _buildSettingsOption('Support', const Color(0xFF007AFF), () {}),
             _buildSettingsOption('Share app', const Color(0xFF007AFF), () {}),
-            _buildSettingsOption('Rate us', const Color(0xFF007AFF), () {}),
-            _buildSettingsOption('Delete account', Colors.red, () {}),
+            _buildSettingsOption(
+                'Rate us', const Color(0xFF007AFF), _showRatingDialog),
+            _buildSettingsOption(
+                'Delete account', Colors.red, _showDeleteAccountDialog),
           ],
         ),
       ),
@@ -73,16 +159,17 @@ class _SettingsViewState extends State<SettingsView> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         TextButton(
-            onPressed: () {},
-            child: Text(
-              title,
-              style: TextStyle(
-                color: color,
-                fontSize: 20,
-                fontFamily: 'SF Pro Text',
-                fontWeight: FontWeight.w400,
-              ),
-            )),
+          onPressed: onTap,
+          child: Text(
+            title,
+            style: TextStyle(
+              color: color,
+              fontSize: 20,
+              fontFamily: 'SF Pro Text',
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
         const Divider(color: Colors.grey),
       ],
     );
